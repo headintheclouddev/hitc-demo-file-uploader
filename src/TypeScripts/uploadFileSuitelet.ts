72c87a14438d814ddb3fae38010e388eaae0c6c7
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2025-07-30 13:16:46 +0100

    Suitelet: Comment what this will do

--- src/TypeScripts/uploadFileSuitelet.ts
+++ src/TypeScripts/uploadFileSuitelet.ts
@@ -9,5 +9,5 @@
 import {EntryPoints} from "N/types";
 
 export function onRequest(context: EntryPoints.Suitelet.onRequestContext) {
-
+  // TODO: Save the files submitted from the online form to the file cabinet and return their internal id
 }
