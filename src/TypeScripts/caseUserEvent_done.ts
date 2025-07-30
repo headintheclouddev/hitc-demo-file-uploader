83521a99b880020eb612c2488b175a06341233c1
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2025-07-30 13:15:51 +0100

    User Event - Done: Implement afterSubmit workaround

--- src/TypeScripts/caseUserEvent_done.ts
+++ src/TypeScripts/caseUserEvent_done.ts
@@ -8,12 +8,27 @@
 
 import {EntryPoints} from "N/types";
 import log = require('N/log');
+import record = require("N/record");
 import runtime = require('N/runtime');
 
 export function beforeSubmit(context: EntryPoints.UserEvent.beforeSubmitContext) {
   log.debug('beforeSubmit', `${runtime.executionContext} ${context.type} case ${context.newRecord.id}`);
   if (context.type == context.UserEventType.CREATE) {
-    context.newRecord.setValue({ fieldId: 'custevent_screenshot', value: context.newRecord.getValue('custevent_screenshot_file_id') });
-    context.newRecord.setValue({ fieldId: 'custevent_execution_logs', value: context.newRecord.getValue('custevent_execution_logs_file_id') });
+    // This seems to not work very well on most accounts.  Why?  Not sure, but afterSubmit method seems to work better.
+    // const screenshotId = context.newRecord.getValue('custevent_screenshot_file_id') as string;
+    // const logsFileId = context.newRecord.getValue('custevent_execution_logs_file_id') as string;
+    // context.newRecord.setValue({ fieldId: 'custevent_screenshot', value: screenshotId });
+    // context.newRecord.setValue({ fieldId: 'custevent_execution_logs', value: logsFileId });
   }
 }
+
+export function afterSubmit(context: EntryPoints.UserEvent.afterSubmitContext) {
+  log.debug('afterSubmit', `${runtime.executionContext} ${context.type} case ${context.newRecord.id}`);
+  if (context.type == context.UserEventType.CREATE) {
+    const screenshotId = context.newRecord.getValue('custevent_screenshot_file_id') as string;
+    const logsFileId = context.newRecord.getValue('custevent_execution_logs_file_id') as string;
+    if (screenshotId || logsFileId)
+      record.submitFields({ type: 'supportcase', id: context.newRecord.id, values: { custevent_screenshot: screenshotId, custevent_execution_logs: logsFileId } });
+  }
+}
+
