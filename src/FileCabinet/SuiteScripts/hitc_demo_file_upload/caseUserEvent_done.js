09b6cdafef0651531039241b6305bcd4adcc5823
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2025-08-05 14:53:32 +0100

    Case - User Event - Done: Re-enable beforeSubmit mode

--- src/FileCabinet/SuiteScripts/hitc_demo_file_upload/caseUserEvent_done.js
+++ src/FileCabinet/SuiteScripts/hitc_demo_file_upload/caseUserEvent_done.js
@@ -5,7 +5,7 @@
  * @NScriptType UserEventScript
  * @NApiVersion 2.1
  */
-define(["require", "exports", "N/log", "N/record", "N/runtime"], function (require, exports, log, record, runtime) {
+define(["require", "exports", "N/log", "N/runtime"], function (require, exports, log, runtime) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     exports.beforeSubmit = beforeSubmit;
@@ -14,19 +14,19 @@ define(["require", "exports", "N/log", "N/record", "N/runtime"], function (requi
         log.debug('beforeSubmit', `${runtime.executionContext} ${context.type} case ${context.newRecord.id}`);
         if (context.type == context.UserEventType.CREATE) {
             // This seems to not work very well on most accounts.  Why?  Not sure, but afterSubmit method seems to work better.
-            // const screenshotId = context.newRecord.getValue('custevent_screenshot_file_id') as string;
-            // const logsFileId = context.newRecord.getValue('custevent_execution_logs_file_id') as string;
-            // context.newRecord.setValue({ fieldId: 'custevent_screenshot', value: screenshotId });
-            // context.newRecord.setValue({ fieldId: 'custevent_execution_logs', value: logsFileId });
+            const screenshotId = context.newRecord.getValue('custevent_screenshot_file_id');
+            const logsFileId = context.newRecord.getValue('custevent_execution_logs_file_id');
+            context.newRecord.setValue({ fieldId: 'custevent_screenshot', value: screenshotId });
+            context.newRecord.setValue({ fieldId: 'custevent_execution_logs', value: logsFileId });
         }
     }
     function afterSubmit(context) {
         log.debug('afterSubmit', `${runtime.executionContext} ${context.type} case ${context.newRecord.id}`);
-        if (context.type == context.UserEventType.CREATE) {
-            const screenshotId = context.newRecord.getValue('custevent_screenshot_file_id');
-            const logsFileId = context.newRecord.getValue('custevent_execution_logs_file_id');
-            if (screenshotId || logsFileId)
-                record.submitFields({ type: 'supportcase', id: context.newRecord.id, values: { custevent_screenshot: screenshotId, custevent_execution_logs: logsFileId } });
-        }
+        // if (context.type == context.UserEventType.CREATE) {
+        //   const screenshotId = context.newRecord.getValue('custevent_screenshot_file_id') as string;
+        //   const logsFileId = context.newRecord.getValue('custevent_execution_logs_file_id') as string;
+        //   if (screenshotId || logsFileId)
+        //     record.submitFields({ type: 'supportcase', id: context.newRecord.id, values: { custevent_screenshot: screenshotId, custevent_execution_logs: logsFileId } });
+        // }
     }
 });
--- src/TypeScripts/caseUserEvent_done.ts
+++ src/TypeScripts/caseUserEvent_done.ts
@@ -8,27 +8,26 @@
 
 import {EntryPoints} from "N/types";
 import log = require('N/log');
-import record = require("N/record");
 import runtime = require('N/runtime');
 
 export function beforeSubmit(context: EntryPoints.UserEvent.beforeSubmitContext) {
   log.debug('beforeSubmit', `${runtime.executionContext} ${context.type} case ${context.newRecord.id}`);
   if (context.type == context.UserEventType.CREATE) {
     // This seems to not work very well on most accounts.  Why?  Not sure, but afterSubmit method seems to work better.
-    // const screenshotId = context.newRecord.getValue('custevent_screenshot_file_id') as string;
-    // const logsFileId = context.newRecord.getValue('custevent_execution_logs_file_id') as string;
-    // context.newRecord.setValue({ fieldId: 'custevent_screenshot', value: screenshotId });
-    // context.newRecord.setValue({ fieldId: 'custevent_execution_logs', value: logsFileId });
+    const screenshotId = context.newRecord.getValue('custevent_screenshot_file_id') as string;
+    const logsFileId = context.newRecord.getValue('custevent_execution_logs_file_id') as string;
+    context.newRecord.setValue({ fieldId: 'custevent_screenshot', value: screenshotId });
+    context.newRecord.setValue({ fieldId: 'custevent_execution_logs', value: logsFileId });
   }
 }
 
 export function afterSubmit(context: EntryPoints.UserEvent.afterSubmitContext) {
   log.debug('afterSubmit', `${runtime.executionContext} ${context.type} case ${context.newRecord.id}`);
-  if (context.type == context.UserEventType.CREATE) {
-    const screenshotId = context.newRecord.getValue('custevent_screenshot_file_id') as string;
-    const logsFileId = context.newRecord.getValue('custevent_execution_logs_file_id') as string;
-    if (screenshotId || logsFileId)
-      record.submitFields({ type: 'supportcase', id: context.newRecord.id, values: { custevent_screenshot: screenshotId, custevent_execution_logs: logsFileId } });
-  }
+  // if (context.type == context.UserEventType.CREATE) {
+  //   const screenshotId = context.newRecord.getValue('custevent_screenshot_file_id') as string;
+  //   const logsFileId = context.newRecord.getValue('custevent_execution_logs_file_id') as string;
+  //   if (screenshotId || logsFileId)
+  //     record.submitFields({ type: 'supportcase', id: context.newRecord.id, values: { custevent_screenshot: screenshotId, custevent_execution_logs: logsFileId } });
+  // }
 }
 
